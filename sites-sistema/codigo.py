a067de6e6f64171d9746de7cfe7679216684ae90
Sabrina Gama <62630517+SabrinaGamaa@users.noreply.github.com>
2025-01-17 18:56:12 -0300

    Teste

---
+++ sites-sistema/codigo.py
@@ -0,0 +1,27 @@
+# Tela inicial:
+    # Título: Hashzap
+    # Botão: Iniciar Chat
+        # quando clicar no botão
+        #Abrir um poppup/model/alerta
+            # Titulo: Bem vindo ao Hashzap
+            # Caixa de texto: Escreva seu nome no chat
+            # Botão entrar no chat
+                # Quando clicar no botao
+                # sumir com o titulo
+                # sumir com o botao iniciar chat
+                #     carregar o chat
+                #     carregar o campo de enviar mensagem: 'digite sua mensagem'
+                #     botão enviar
+                #         quando clicar no botao enviar 
+                #         enviar a mensagem
+                #         limpar a caixa de mensagem
+
+# Flet
+import flet as ft
+# Criar uma funcao principal para rodae o seu 
+def main():
+    # Todos tenho que colocar o ft
+
+# executar essa função com o flet
+
+ft.app(main)
